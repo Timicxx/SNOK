44f9466581e25d9190b134876622976bc6cee27e
Tymoteusz Grabowski <tymek1rt@hotmail.com>
2019-01-16 09:30:56 +0100

    Fix

--- Debugger.cpp
+++ Debugger.cpp
@@ -6,6 +6,8 @@ Debugger::Debugger(GameManager* gameManager) {
 
 void Debugger::Debug() {
 	// Output player coordinates to serial console
+	// int _X = gameManager->getPlayer()->getPositionX();
+	// int _Y = gameManager->getPlayer()->getPositionY();
 	int _X = gameManager->getPlayer()->getPositions()[0];
 	int _Y = gameManager->getPlayer()->getPositions()[1];
 	String output = "X: " + String(_X) + " | " + "Y: " + String(_Y);
